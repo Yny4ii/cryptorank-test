75f8b59d8fca70d59b4ab08e141aef94758e05ed
ilya <>
2024-01-18 12:30:01 +0300

    fix: api url

--- src/hooks/useGetCurrencies.ts
+++ src/hooks/useGetCurrencies.ts
@@ -6,7 +6,7 @@ const API = "https://api.cryptorank.io/v1/currencies/";
 const API_KEY = "9f25a746705f0ac50d0d7a3c2ad2c2bfd82079004491ebb51e3fa337ba81";
 export const useGetCurrencies = (limit?: number, offset?: number) => {
   const { data, error, isLoading } = useSWR<GetCurrencies>(
-    `${API}/?api_key=${API_KEY}&limit=${limit}&offset=${offset}`,
+    `${API}?api_key=${API_KEY}&limit=${limit}&offset=${offset}`,
     fetcher
   );
   return {
